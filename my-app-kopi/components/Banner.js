d8beeb6825c24fc8fd86f0290ccf9d13654588e9
SussiRK <131151968+SussiRK@users.noreply.github.com>
2024-12-15 22:50:37 +0100

    Rykket banner på chat + kommentarer

--- my-app-kopi/components/Banner.js
+++ my-app-kopi/components/Banner.js
@@ -1,17 +1,17 @@
-// Importerer nødvendige komponenter
+// Importerer komponenter
 import React from 'react';
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
-// Funktionel komponent for banneret
-export default function Banner({ onPress }) {
+// Funktionel komponent for Banner
+export default function Banner({ onPress, style }) {
   return (
     // TouchableOpacity gør banneret trykbart og udfører en funktion når det trykkes på
-    <TouchableOpacity onPress={onPress} style={styles.bannerContainer}>
-      {/* Viser billedet som bliver hentet fra placeringen */}
+    <TouchableOpacity onPress={onPress} style={[styles.bannerContainer, style]}>
+      {/* Billedet der vises som banner */}
       <Image
-        source={require('../assets/banner.png')} // Hvor bannerbilledet er gemt
-        style={styles.bannerImage} // Anvender global styles til billedet
-        resizeMode="cover" // Justerer billedet så det dækker hele rammen 
+        source={require('../assets/banner.png')}
+        style={styles.bannerImage}
+        resizeMode="cover"
       />
     </TouchableOpacity>
   );
@@ -20,13 +20,13 @@ export default function Banner({ onPress }) {
 const styles = StyleSheet.create({
   bannerContainer: {
     position: 'absolute',
-    bottom: 80,
     left: 30,
     right: 30,
     height: 60,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
+    borderRadius: 5,
     borderTopWidth: 1,
     borderTopColor: '#ccc',
     zIndex: 10,
